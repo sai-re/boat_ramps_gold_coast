bc97102a1ef5562ade530f14f88ba4894253b712
Siavoush Redhai <SiavoushRedhai@siavoushs-mbp.mynet>
2020-10-29 23:14:21 +0000

    complete tests for fetch data component

--- client/src/components/tests/FetchData.test.js
+++ client/src/components/tests/FetchData.test.js
@@ -1,24 +1,62 @@
-//general
-//test all component load
-
-//behavioral
-//test restet button working - correct actions
-//test clicking on map sends correct action
-
-//unit
-//test components has correct props
-
-//redux
-//test redux thunk fetch
-//test action creators
-//test reducer function
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import { FetchData } from '../FetchData';
+
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../../js/reducers/index';
+import thunk from "redux-thunk";
+import ConnectedFetchData, { FetchData } from '../FetchData';
 
 it("renders without crashing", () => {
     const wrapper = shallow(<FetchData />);
     const component = wrapper.find(".chart__container");
     expect(component).toHaveLength(1);
+});
+
+describe("has actions from redux as props", () => {
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const wrapper = shallow(<ConnectedFetchData store={store} />).dive().dive();
+    const props = wrapper.instance().props;
+
+    it("has getData action", () => {
+        expect(props.getData).toBeInstanceOf(Function);
+    });
+
+    it("has filterMaterial action", () => {
+        expect(props.filterMaterial).toBeInstanceOf(Function);
+    });
+
+    it("has filterSize action", () => {
+        expect(props.filterSize).toBeInstanceOf(Function);
+    });
+
+    it("has resetMap action", () => {
+        expect(props.resetMap).toBeInstanceOf(Function);
+    });
+});
+
+describe("component props test", () => {
+    const getDataMock = jest.fn();
+    const resetMapMock = jest.fn();
+
+    const props = {
+        getData: getDataMock,
+        resetMap: resetMapMock
+    };
+
+    const wrapper = shallow(<FetchData {...props} />);
+
+    it("runs getData action on component mount", () => {
+        wrapper.instance().componentDidMount();
+        const callCount = getDataMock.mock.calls.length;
+        expect(callCount).toBeGreaterThan(1);
+    });
+    
+    it("runs action on reset", () => { 
+        const wrapper = shallow(<FetchData {...props} />);
+        const button = wrapper.find(".button");
+        button.simulate("click");
+    
+        const callCount = resetMapMock.mock.calls.length;
+        expect(callCount).toBe(1);
+    });
 });
